8414d255cff55ca0310daeac8f11334d236083cc
twk <rhsh153@naver.com>
2022-08-03 19:17:33 +0000

    init

--- django/django_swagger/sample_swagger/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SampleSwaggerConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'sample_swagger'
--- django/django_swagger/sample_swagger/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-import sample_swagger.views as views
-urlpatterns =[
-    path('mysql/',views.mysql,name='mysql'),
-    path('producer/',views.producer,name='producer'),
-    path('consumer/',views.consumer,name='consumer'),
-    path('dbinput/',views.dbinput,name='dbinput'),]
